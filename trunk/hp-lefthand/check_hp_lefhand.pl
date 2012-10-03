5e1a67d63f4b45fba036c7232fdd250376d0841d
yannig.perre <yannig.perre@e5bd46af-f3c3-3ad3-dd7f-be803185b5b6>
2012-10-03 15:43:46 +0000

    Début de gestion des volumes.

--- trunk/hp-lefthand/check_hp_lefhand.pl
+++ trunk/hp-lefthand/check_hp_lefhand.pl
@@ -31,13 +31,14 @@ my $parse_status = GetOptions(
     'element=s'         => \$element,
     'cluster=s'         => \$element_name,
     'module=s'          => \$element_name,
+    'volume=s'          => \$volume_name,
     'list'              => \$list,
     'c|critical=s'      => \$critical,
     'w|warning=s'       => \$warning,
 );
 
 sub usage {
-  print "$0 [--help] -H <HOST> --element {cluster|module} [--cluster <CLUSTER>] [--module <MODULE>]
+  print "$0 [--help] -H <HOST> --element {cluster|module} [--cluster <CLUSTER>] [--module <MODULE>] [--volume <VOLUME>]
   [--list] [-C <COMMUNITY>] [-v <SNMPVERSION>] [-p <PORT>]
 
 Examples:
@@ -109,6 +110,21 @@ my $oid_module_name                   = "1.3.6.1.4.1.9804.3.1.1.2.12.46.1.2";
 my $oid_cluster_module_storage_status = "1.3.6.1.4.1.9804.3.1.1.2.12.46.1.10";
 my $oid_module_usable_space           = "1.3.6.1.4.1.9804.3.1.1.2.12.46.1.5";
 my $oid_module_used_space             = "1.3.6.1.4.1.9804.3.1.1.2.12.46.1.29";
+# Volume OIDs
+my $oid_volume_name                 = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.2";
+my $oid_volume_cluster_name         = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.11";
+my $oid_volume_used_space           = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.31";
+my $oid_volume_available_space      = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.51";
+my $oid_volume_used_percent         = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.52";
+my $oid_volume_stats_IOsRead        = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.36";
+my $oid_volume_stats_IOsWrite       = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.37";
+my $oid_volume_stats_BytesRead      = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.38";
+my $oid_volume_stats_BytesWrite     = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.39";
+my $oid_volume_stats_QDepthRead     = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.40";
+my $oid_volume_stats_QDepthWrite    = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.41";
+my $oid_volume_stats_IoLatencyRead  = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.42";
+my $oid_volume_stats_IoLatencyWrite = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.43";
+my $oid_volume_stats_CacheHits      = "1.3.6.1.4.1.9804.3.1.1.2.12.97.1.44";
 
 my $response;
 my %resp;
@@ -200,6 +216,66 @@ sub test_cluster {
   }
 }
 
+# Test volume status
+sub test_volume {
+  my %volume_name = ();
+  my %id_to_volume_name = ();
+  $response = snmp_get_table($oid_volume_name);
+  %resp = %{$response};
+  # Get all volume from all cluster
+  foreach my $key(keys(%resp)) {
+    print "Storage Volume name - $key = ".$resp{$key}."\n" if($verbose || $list);
+    if($key =~ /(\d+)$/) {
+      $id_to_volume_name{$1} = $resp{$key};
+    }
+  }
+  $response = snmp_get_table($oid_volume_cluster_name);
+  %resp = %{$response};
+  # Ignoring volume from other cluster
+  foreach my $key(keys(%resp)) {
+    print "Storage Volume cluster name - $key = ".$resp{$key}."\n" if($verbose || $list);
+    if($element_name eq $resp{$key}) {
+      next if($volume_name && !($resp{$key} =~ /$volume_name/));
+      if($key =~ /(\d+)$/) {
+        print "Adding new volume to test (".$id_to_volume_name{$1}.")\n" if($verbose || $list);
+        $volume_name{$1} = $id_to_volume_name{$1};
+      }
+    }
+  }
+  # Get used space
+  $response = snmp_get_table($oid_volume_used_space);
+  %resp = %{$response};
+  my %used_space = ();
+  foreach my $key(keys(%resp)) {
+    if($key =~ /(\d+)$/) {
+      next if(!$volume_name{$1});
+      print "Volume used space - $key (".$volume_name{$1}.")= ".$resp{$key}."\n" if($verbose || $list);
+      $used_space{$1} = $resp{$key};
+    }
+  }
+  # Get percent used
+  $response = snmp_get_table($oid_volume_used_percent);
+  %resp = %{$response};
+  my $percent_used = 0;
+  my $total_space = 0;
+  foreach my $key(keys(%resp)) {
+    if($key =~ /(\d+)$/) {
+      next if(!$volume_name{$1});
+      $percent_used = $resp{$key};
+      print "Volume percent used - $key ($percent_used)= ".$resp{$key}."\n" if($verbose || $list);
+      $total_space = int($used_space{$1} / $percent_used * 100 + 0.5);
+      my $warning_value  = ($warning  ? int($warning  * $total_space / 100 + 0.5) : "");
+      my $critical_value = ($critical ? int($critical * $total_space / 100 + 0.5) : "");
+      if($critical && $critical < $percent_used) {
+        $status = 2;
+      } elsif($warning && $warning < $percent_used) {
+        $status = 1;
+      }
+      push(@perfdata, $volume_name{$1}."=".$used_space{$1}."kB;$warning_value;$critical_value;0;$total_space");
+    }
+  }
+}
+
 # Test module status
 sub test_module {
   # Get storage module status
@@ -249,6 +325,8 @@ if($element eq "cluster") {
   test_cluster();
 } elsif($element eq "module") {
   test_module();
+} elsif($element eq "volume") {
+  test_volume();
 } else {
   print "Unknown module type specified: $element\n";
   exit(3);
